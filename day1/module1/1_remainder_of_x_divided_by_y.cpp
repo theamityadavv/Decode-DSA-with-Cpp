957bfa31d66d0c663e8445feaecbe9dea84e70f7
Amit Yadav <133171339+theamityadavv@users.noreply.github.com>
2025-04-08 23:46:30 +0530

    Create 1_remainder_of_x_divided_by_y.cpp

---
+++ day1/module1/1_remainder_of_x_divided_by_y.cpp
@@ -0,0 +1,12 @@
+//Q1. Take two integers input, x and y : x>y, and find the remainder when x is divided by y.
+#include<iostream>
+using namespace std;
+int main(){
+	int x,y;
+	cout<<"enter the first number\n";
+	cin>>x;
+	cout<<"enter the second number\n";
+	cin>>y;
+	cout<<"the remainder is " <<x%y <<" when x is divided by y\n";
+	return 0;
+}
